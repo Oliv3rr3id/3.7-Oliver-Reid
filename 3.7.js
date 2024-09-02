bdd0fe71d92c3d929a9cb82d3f273c478d7921bc
Oliv3rr3id <oliverr.20125@obhs.school.nz>
2024-09-03 09:47:50 +1200

    Add files via upload

--- 3.7.js
+++ 3.7.js
@@ -2,24 +2,31 @@
 alert("js attatached");
 var totalCost = 0;
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
-if (dd < 10) {
-    dd = '0' + dd
-}if (mm < 10) {
-    mm = '0' + mm
-}today = yyyy + '-' + mm + '-' + dd;
-document.getElementById("dateInput").setAttribute("min", today);
-
 function boatBooking(){
 	var checkInDate = document.getElementById("dateInput").value;
-	alert(checkInDate);
-	var amountOfHours = document.getElementById("hoursInput").value;
-	alert(amountOfHours);
-	var costofReservation = this.dataset.price;
+	if (document.getElementById("dateInput").validity.valueMissing) {
+		alert("Please enter a valid first name");
+		document.getElementById("errorMessage1").innerHTML = "Please enter a valid date";
+		return;
+	} else {
+		document.getElementById("errorMessage1").innerHTML = "";
+	}var amountOfHours = document.getElementById("hoursInput").value;
+	if (document.getElementById("hoursInput").validity.valueMissing ||document.getElementById("hoursInput").validity.rangeUnderflow ||document.getElementById("hoursInput").validity.rangeOverflow) {
+		alert("Please enter a valid amount");
+		document.getElementById("errorMessage2").innerHTML = "Please enter a valid amount of hours";
+		return;
+	} else {
+		document.getElementById("errorMessage2").innerHTML = "";
+	}var amountofGuests = document.getElementById("guestInput").value;
+	if (document.getElementById("guestInput").validity.valueMissing ||document.getElementById("guestInput").validity.rangeUnderflow ||document.getElementById("guestInput").validity.rangeOverflow) {
+		alert("Please enter a valid amount");
+		document.getElementById("errorMessage3").innerHTML = "Please enter a valid amount of hours";
+		return;
+	} else {
+		document.getElementById("errorMessage3").innerHTML = "";
+	}var costofReservation = this.dataset.price;
 	alert(costofReservation);
+	
 	var boatTime = this.dataset.value;
 	alert(boatTime);
 	
@@ -35,26 +42,17 @@ function boatBooking(){
 		}
 	}
 	totalCost += Number(costofReservation) + Number(costOfExtras);
-	outputSummary(checkInDate, amountOfHours, boatTime, costofReservation, extrasOption, costOfExtras)
-}
-
-function outputSummary(checkInDate, amountOfHours, boatTime, costofReservation, extrasOption, costOfExtras){
 	outDate.innerHTML = checkInDate;
 	outHours.innerHTML = amountOfHours;
 	outTime.innerHTML = boatTime;
 	outPrice.innerHTML = "$" + costofReservation;
 	outExtras.innerHTML = extrasOption;
-	outExtrasPrice.innerHTML = costOfExtras;
+	outExtrasPrice.innerHTML = "$" + costOfExtras;
 	outTotal.innerHTML = totalCost;
 	checkInputs(checkInDate, amountOfHours, boatTime, costofReservation, extrasOption, costOfExtras)
 }
 
-var card = document.getElementsByClassName("card");
-for (var i = 0; i < card.length; i++) {
-	card[i].addEventListener("click", boatBooking);
-}
-
-function checkInputs() {
+function checkInputs(checkInDate, amountOfHours, boatTime, costofReservation, extrasOption, costOfExtras){
 	 if (termsAndConditions.checked) {
         alert("Terms box is checked");
     } else {
@@ -66,16 +64,58 @@ function checkInputs() {
     var age = document.getElementById('ageInput').value;
 	var license = document.getElementById('licenseInput').value;
 	var cellPhone = document.getElementById('cellphoneInput').value;
+	var email = document.getElementById('emailInput').value;
 	//store all of the customer details as variables
-	alert(firstName + lastName + age + email);
-    alert(checkInDate)
+	alert(firstName + " " + lastName + " " + age + " " + license + " " + cellPhone + " " + email);
+	pushData(firstName, lastName, age, license, cellPhone, email, checkInDate, amountOfHours, boatTime, costofReservation, extrasOption, costOfExtras)
 }
 
+function pushData(firstName, lastName, age, license, cellPhone, email, checkInDate, amountOfHours, boatTime, costofReservation, extrasOption, costOfExtras) {
+    alert("At the push data function - nearly done!");
+    console.log("myFunction fired.");
+    console.log("Getting Values....");
+    console.log("Initialing Airtable API....");
+    var Airtable = require('airtable');
+    var base = new Airtable({
+        apiKey: 'patIPemM83ckitTE3.853907fa0fcd0d91559d698c1062d6f651febd7bb7d01c10d24c0042b8dcd3b8'
+    }).base('appFd8HSYm7rdBSxt');
+    console.log("Creating a record....");
+    base('Boat Booking').create({
+            "First Name": firstName,
+			"Last Name": lastName,
+			"Age": age,
+			"License": license,
+			"Cell Phone": cellPhone,
+			"Email": email
 
+        }, {
+            typecast: true
+        },
+        function(err, record) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            alert("record created");
+            console.log("Record created: " + record.getId());
+        });
+}
 
+var card = document.getElementsByClassName("card");
+for (var i = 0; i < card.length; i++) {
+	card[i].addEventListener("click", boatBooking);
+}
 
-
-
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd
+}if (mm < 10) {
+    mm = '0' + mm
+}today = yyyy + '-' + mm + '-' + dd;
+document.getElementById("dateInput").setAttribute("min", today);
 
 
 
